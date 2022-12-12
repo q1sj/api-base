f9544ee6e307111c6af7f1dac7732fc62e9b8638
Q1sj <q1sj@outlook.com>
2022-12-12 09:48:52 +0800

    readClassPathResourceFile

---
+++ xsy-api-demo/src/main/java/com/xsy/api/common/config/TestReadResourceController.java
@@ -0,0 +1,34 @@
+package com.xsy.api.common.config;
+
+import com.xsy.base.util.FileUtils;
+import com.xsy.security.annotation.NoAuth;
+import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Q1sj
+ * @date 2022.12.12 9:20
+ */
+@RestController
+@RequestMapping("/test/resource")
+public class TestReadResourceController {
+    @NoAuth
+    @GetMapping("/read")
+    public void read(String path, HttpServletResponse response) throws IOException {
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        String[] split = path.split("/");
+        response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(split[split.length - 1], StandardCharsets.UTF_8.displayName()));
+        byte[] bytes = FileUtils.readClassPathResourceFile(path);
+        response.getOutputStream().write(bytes);
+    }
+}
--- xsy-common/src/main/java/com/xsy/base/util/FileUtils.java
+++ xsy-common/src/main/java/com/xsy/base/util/FileUtils.java
@@ -1,8 +1,29 @@
 package com.xsy.base.util;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author Q1sj
  * @date 2022.9.26 14:54
  */
 public class FileUtils extends org.apache.commons.io.FileUtils {
+    /**
+     * 读取resource目录下文件
+     * 不要以/开头
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static byte[] readClassPathResourceFile(String path) throws IOException {
+        BizAssertUtils.isNotBlank(path, "path not blank");
+        ClassPathResource classPathResource = new ClassPathResource(path);
+        try (InputStream inputStream = classPathResource.getInputStream()) {
+            return IOUtils.readFully(inputStream, inputStream.available());
+        }
+    }
 }
