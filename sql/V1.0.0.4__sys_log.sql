36959e211d9f818d8a7b054feb66397d2b721d2c
Q1sj <q1sj@outlook.com>
2023-12-27 10:41:44 +0800

    数据库数据重复校验工具

---
+++ sql/V1.0.0.4__sys_log.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `sys_log`
+(
+    `id`          bigint(20)   NOT NULL,
+    `method`      varchar(255) NOT NULL,
+    `ip`          varchar(15)  DEFAULT NULL,
+    `username`    varchar(255) DEFAULT NULL,
+    `url`         varchar(255) DEFAULT NULL,
+    `args`        text,
+    `resp`        text,
+    `throwable`   text         DEFAULT NULL,
+    `cost`        bigint(20)   NOT NULL,
+    `record_time` datetime     NOT NULL,
+    `create_time` datetime     NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_record_time` (`record_time`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
